976f64d36b6a19df1da6f59fe1c4e0400a53e776
RodrigoT0rres <160190251+RodrigoT0rres@users.noreply.github.com>
2024-02-16 20:39:10 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,15 @@
+let nome = 'rodrigo'
+var total = 50 + 5 * 2 / 2 * 20; 
+var total2 = 50 - 5 + 2 * 2 * 10; 
+var total3 = 90 + 5 * 9 / 9 * 40; 
+var n1 = "200";
+var n2 = 50; 
+var numero = '80'
+var unidade = 'kg'
+var peso = numero + unidade;
+var pesoPorDois = numero / 2+unidade;
+var incremento = 5;
+console.log(+n1+n2)
+console.log(isNaN(nome + total))
+console.log(++incremento)
+console.log(pesoPorDois)
